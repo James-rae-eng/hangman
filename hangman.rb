410ac28c5afca669f3a6956155e582b8ba303eaf
James Rae <jrae151515@yahoo.co.uk>
2022-04-25 18:47:25 +0100

    Added delete save function, general tidy up, core game now complete.

--- hangman.rb
+++ hangman.rb
@@ -11,7 +11,6 @@ class Computer
             word = (File.readlines("google-10000-english-no-swears.txt").sample)
         end
         @word = word.split("")
-        print @word.join
 
         (@word.length - 1).times {@empty_word.push("_")}
         puts "The computer has selected the word below:\n"
@@ -20,15 +19,15 @@ class Computer
 end
 
 class User
-    def initialize
-        @number_of_guesses = 0
-        @running_word = nil
-        @word = nil
+    def initialize (word, empty_word, number_of_guesses)
+        @word = word
+        @running_word = empty_word
+        @number_of_guesses = number_of_guesses
     end
 
     def guess
-        if @number_of_guesses >= 1 
-            puts "\nPlease type a character to guess. Type '#s' to save current game."
+        if @number_of_guesses >= 0 
+            puts "\nPlease type a character to guess."
             while guess = gets.chomp 
                 case 
                 when guess.length == 1 && guess.match(/^[a-zA-Z]+$/)
@@ -42,7 +41,7 @@ class User
                 end
             end
         else 
-            print "You're all out of guesses, game over, better luck next time"
+            print "\nGame over, You're all out of guesses. The word was: #{@word.join("")}"
         end
     end
 
@@ -54,7 +53,6 @@ class User
         end 
         puts "Update: #{@running_word.join(" ")}"
     end
-
 
     def guess_correct?(guess)
         if @word.include?(guess)
@@ -73,9 +71,6 @@ class User
     end
 
     def save_game
-        #ask user for save info
-        #create txt file with incrementing number
-        #add in all @objects to file
         puts "Please enter a name for your save (this can be anything)."
         save_name = gets.chomp.to_s.downcase
 
@@ -86,7 +81,7 @@ class User
             file.puts @running_word.join("")
             file.puts @word.join("")
         end
-        #ask if user wants to resume the game or quit
+        
         puts "Game state saved, you can close the program or enter 'e' to exit,
         Alternatively press 'r' to resume the game."
         choice = gets.chomp.to_s.downcase
@@ -98,13 +93,6 @@ class User
             puts "Invalid selection"
         end
     end
-
-    def play(word, empty_word, number_of_guesses) 
-        @number_of_guesses = number_of_guesses
-        @running_word = empty_word
-        @word = word
-        guess()
-    end
 end
     
 class Game 
@@ -112,9 +100,9 @@ puts "\n\e[1m\e[4mWelcome to Hangman\e[0m"
 puts "\n\e[4mHow to play\e[0m:
 In this game you play against the computer which selects a random 5-12 character word.
 You'll have 6 guesses to get the word (1 letter at a time).\n"
+puts "You can type '#s' at any point to save current game."
     def initialize
         @computer = Computer.new
-        @user= User.new
     end
 
     def replay_game
@@ -130,18 +118,7 @@ You'll have 6 guesses to get the word (1 letter at a time).\n"
         end
     end
 
-    def load_save
-        list_of_saves = Dir["saves/**/*.txt"].map.with_index do |name, index|
-          clean_name = name.gsub("saves/", "").gsub(".txt", "")
-          list_name = "#{index + 1}. #{clean_name}"
-        end
-        puts list_of_saves
-        #ask user to select a save using the index number (need to -1)
-        #retrieve the file with that name 
-        #read lines of the save and store in objects
-        #pass the objects to start the user playing
-        puts "Please type the number of the save file you wish to load"
-        save_num = gets.chomp.to_i
+    def load_save(list_of_saves, save_num)
         save_file = "saves/#{list_of_saves[save_num-1][3..-1]}.txt"
 
         number_of_guesses = IO.readlines(save_file)[0].to_i
@@ -152,31 +129,62 @@ You'll have 6 guesses to get the word (1 letter at a time).\n"
         word = word_unformat[0...-1].split("")
 
         puts "The partial word is: #{running_word.join(" ")}"
-        @user.play(word, running_word, number_of_guesses)
+        @user= User.new(word, running_word, number_of_guesses)
+        @user.guess
+    end
+
+    def delete_save(list_of_saves, save_num)
+        save_file = "saves/#{list_of_saves[save_num-1][3..-1]}.txt"
+        File.delete(save_file)
+        puts "Save deleted"
+        start()
+    end
+
+    def manage_save
+        list_of_saves = Dir["saves/**/*.txt"].map.with_index do |name, index|
+          clean_name = name.gsub("saves/", "").gsub(".txt", "")
+          list_name = "#{index + 1}. #{clean_name}"
+        end
+        puts list_of_saves
+        puts "Please type the number of the save file you wish to load"
+        puts "Alternatively type the save number followed by 'd' to delete a file (e.g. 2d would delete the 2nd save)."
+        
+        while choice = gets.chomp
+            case
+            when choice.length == 1 && Integer(choice, exception: false)
+                load_save(list_of_saves, choice.to_i)
+                break
+            when choice.length == 2
+                delete_save(list_of_saves, choice.chop.to_i)
+                break
+            else 
+                puts "Invalid entry, please try again"
+            end
+        end
     end
 
     def new_game
         puts "New game, Press Enter to continue"
         STDIN.getc
         @computer.play
-        @user.play(@computer.word, @computer.empty_word, 6)
+        @user= User.new(@computer.word, @computer.empty_word, 6)
+        @user.guess
     end
 
     def start
-        puts "Would you like to load a previous game from a save file? Enter y / n"
+        puts "\nWould you like to load or manage a previous game from a save file? Enter y / n"
         choice = gets.chomp.to_s.downcase
         if choice == "y" && Dir["saves/*.txt"].empty?
             puts "No save files found"
             new_game()
         elsif choice == "y" && !Dir["saves/*.txt"].empty?
-            load_save()
+            manage_save()
         else 
             new_game()   
         end
         replay_game()
     end
 end  
-
 
 game = Game.new
 game.start
